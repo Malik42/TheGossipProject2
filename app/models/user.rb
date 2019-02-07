5ccc087561c72d07d72a3b96ff0068cf6938dfc5
Modeuil <votre_email@votre_email.com>
2019-02-07 12:31:46 +0100

    creation validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,14 @@ class User < ApplicationRecord
                        :length => {:within => 6..40},
                        :allow_blank => true,
                        :on => :update
-    
-  
-  
+  validates :email,    :presence => true,
+                       :uniqueness => true,
+                       :format =>{ with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+
+                       
+
+
+
   belongs_to :city, optional: true # on fait le lien entre l'user et sa ville
   has_many :gossips # on dit qu'un user peut cree des gossip
   has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage" # on fait un lien entre l'utilisateur et les messages qu'il envoi
