1f7b4aa2aaac51117c63f7c2dadcaf7433578085
Modeuil <votre_email@votre_email.com>
2019-02-07 12:39:38 +0100

    create validates first_name, last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   validates :email,    :presence => true,
                        :uniqueness => true,
                        :format =>{ with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+                       validates :first_name,:last_name, length: { minimum: 2 }
 
                        
 
