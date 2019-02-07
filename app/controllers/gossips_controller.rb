c274dcfe56a81107bccbbc4d96fa004e0adc68d9
Malik42 <malik.ahres@hotmail.fr>
2019-02-07 11:46:43 +0100

    methdo edit and update finish

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -31,12 +31,17 @@ class GossipsController < ApplicationController
 
   def edit
     # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
+    @gossip = Gossip.find(params[:id])
   end
 
   def update
     # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
     # pour info, le contenu de ce formulaire sera accessible dans le hash params
     # Une fois la modification faite, on redirige généralement vers la méthode show (pour afficher le potin modifié)
+    @gossip = Gossip.find(params[:id])
+    post_params = params.require(:gossip).permit(:title, :content)
+    @gossip.update(post_params)
+    redirect_to gossip_path
   end
 
   def destroy
